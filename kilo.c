16eca9afdf6e6da4a04b069ec7831b0bc61242ce
Adit <aditsharma1203@gmail.com>
2023-01-16 13:15:15 +0530

    Tested out Cannonical Mode

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -18,7 +20,7 @@ void enableRawMode()
 	atexit( disableRawMode);
 
 	raw = original_termios;
-	raw.c_lflag &= ~(ECHO);
+	raw.c_lflag &= ~( ECHO | ICANON );
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw );
 }
 
@@ -27,6 +29,11 @@ int main (int argc, char *argv[])
 	enableRawMode();
 
 	char c;
-	while ( read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	while ( read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+		if( iscntrl(c) )
+			printf("%d\n", c );
+		else
+			printf("%d %c\n", c, c );
+	}
 	return 0;
 }
