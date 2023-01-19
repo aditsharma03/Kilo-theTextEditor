cab698dbe4b58995c613406161eca6cfe285b77a
Adit <aditsharma1203@gmail.com>
2023-01-19 22:33:39 +0530

    f

--- kilo.c
+++ kilo.c
@@ -7,6 +7,7 @@
 #include <termios.h>
 #include <unistd.h>
 #include <errno.h>
+#include <string.h>
 
 /*** data ***/ 
 
@@ -72,6 +73,30 @@ int getWindowSize( int *rows, int *cols )
 	*rows = ws.ws_row;
 	*cols = ws.ws_col;
 	return 0;
+}
+
+/**** append buffer ****/ 
+
+struct abuf{
+	char *b;
+	int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend( struct abuf *ab, const char *s, int len )
+{
+	char *new = realloc( ab->b, ab->len + len );
+
+	if( new == NULL ) return;
+	memcpy( &new[ab->len], s, len );
+	ab->b = new;
+	ab->len += len;
+}
+
+void abFree( struct abuf *ab )
+{
+	free( ab->b );
 }
 
 /*** input ***/ 
